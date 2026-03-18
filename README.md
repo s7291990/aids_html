14485191e7c9566ff108d25fa7780a8d9817b396
양승진 <7291990@daum.net>
2026-03-18 16:11:01 +0900

    feat: 파일추가

---
+++ README.md
@@ -0,0 +1,6 @@
+## 그리드 라이브러리
++ https://datatables.net
+## 차트 라이브러리
++ 페이지목록 정보 : /accident-partner/work.html
+
+
---
+++ accident-manager/work.js
@@ -0,0 +1,114 @@
+var work = [
+  /*기타*/
+  { "cate":"m01", "dep01":"로그인", "dep02":"로그인", "url":"./login/login.html", "date":"2025-08-10" },
+
+  { "cate":"m02", "dep01":"홈", "dep02":"홈", "url":"./home/index.html", "date":"2025-08-10" },
+
+  { "cate":"m03", "dep01":"업체관리", "dep02":"업체관리 목록", "url":"./business/biz-list.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"업체관리 수정", "url":"./business/biz-edit.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"업체 캐시포인트 관리", "url":"./business/cash-point-list.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"업체 캐시포인트 등록", "url":"./business/cash-point-register.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"구획별 업체리스트", "url":"./business/compartment-list.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"구획직영점 관리 등록", "url":"./business/compartment-register.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"사업자 정보 관리 등록", "url":"./business/entrepreneur-register.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"[팝업]탈퇴업체 삭제", "url":"./business/popup-delete.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"탈퇴업체 관리", "url":"./business/withdrawal-list.html", "date":"2025-08-10" },
+  { "cate":"m03", "dep01":"업체관리", "dep02":"탈퇴업체 수정", "url":"./business/withdrawal-register.html", "date":"2025-08-10" },
+
+  { "cate":"m04", "dep01":"차량 관리", "dep02":"차량 관리", "url":"./car/car-list.html", "date":"2025-08-10" },
+  { "cate":"m04", "dep01":"차량 관리", "dep02":"차량수정", "url":"./car/car-edit.html", "date":"2025-08-10" },
+  { "cate":"m04", "dep01":"차량 관리", "dep02":"[팝업]차량 엑셀 등록", "url":"./car/popup-excel-register.html", "date":"2025-08-10" },
+
+  { "cate":"m05", "dep01":"제휴 관리", "dep02":"제휴 관리", "url":"./partnership/partnership-list.html", "date":"2025-08-10" },
+  { "cate":"m05", "dep01":"제휴 관리", "dep02":"제휴콜 자동요청 대기중", "url":"./partnership/partnership-register.html", "date":"2025-08-10" },
+  { "cate":"m05", "dep01":"제휴 관리", "dep02":"[팝업]엑셀 다운로드", "url":"./partnership/popup-excel-download.html", "date":"2025-08-10" },
+
+  { "cate":"m06", "dep01":"보험 계약서", "dep02":"보험 계약서", "url":"./contract/contract-list.html", "date":"2025-08-10" },
+  { "cate":"m06", "dep01":"보험 계약서", "dep02":"보험계약서 등록", "url":"./contract/contract-register.html", "date":"2025-08-10" },
+  { "cate":"m06", "dep01":"보험 계약서", "dep02":"[팝업]업체 선택", "url":"./contract/popup-biz-select.html", "date":"2025-08-10" },
+  { "cate":"m06", "dep01":"보험 계약서", "dep02":"[팝업]비대면계약서 작성요청", "url":"./contract/popup-contact-register.html", "date":"2025-08-10" },
+
+  { "cate":"m07", "dep01":"일반 계약서", "dep02":"일반 계약서", "url":"./contract-normal/contract-list.html", "date":"2025-08-10" },
+
+  { "cate":"m08", "dep01":"직원 관리", "dep02":"직원 관리", "url":"./staff/staff-list.html", "date":"2025-08-10" },
+  { "cate":"m08", "dep01":"직원 관리", "dep02":"업체관리", "url":"./staff/staff-register.html", "date":"2025-08-10" },
+  { "cate":"m08", "dep01":"직원 관리", "dep02":"탈퇴직원 관리", "url":"./staff/withdrawal-list.html", "date":"2025-08-10" },
+
+  { "cate":"m09", "dep01":"소개처 관리", "dep02":"소개처 관리", "url":"./referral/referral-list.html", "date":"2025-08-10" },
+  { "cate":"m09", "dep01":"소개처 관리", "dep02":"소개처 등록", "url":"./referral/referral-register.html", "date":"2025-08-10" },
+
+  { "cate":"m010", "dep01":"배너 관리", "dep02":"배너관리", "url":"./banner/banner-list.html", "date":"2025-08-10" },
+  { "cate":"m010", "dep01":"배너 관리", "dep02":"배너 등록", "url":"./banner/banner-register.html", "date":"2025-08-10" },
+
+  { "cate":"m011", "dep01":"공지/문의", "dep02":"문의하기 목록", "url":"./notice/contact-list.html", "date":"2025-08-10" },
+  { "cate":"m011", "dep01":"공지/문의", "dep02":"문의관리 등록", "url":"./notice/contact-register.html", "date":"2025-08-10" },
+  { "cate":"m011", "dep01":"공지/문의", "dep02":"공지사항 목록", "url":"./notice/notice-list.html", "date":"2025-08-10" },
+  { "cate":"m011", "dep01":"공지/문의", "dep02":"공지사항 등록", "url":"./notice/notice-register.html", "date":"2025-08-10" },
+  { "cate":"m011", "dep01":"공지/문의", "dep02":"공지사항 상세", "url":"./notice/notice-view.html", "date":"2025-08-10" },
+
+  { "cate":"m012", "dep01":"설정", "dep02":"관리자 정보", "url":"./setting/admin-register.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"자동차 데이터 관리", "url":"./setting/car-data-list.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"자동차 데이터 등록", "url":"./setting/car-data-register.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"청구기준 관리", "url":"./setting/claim-list.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"청구기준 관리 등록", "url":"./setting/claim-register.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"보험사 코드 관리", "url":"./setting/code-list.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"보험사 코드 등록", "url":"./setting/code-register.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"구획 수수료 관리", "url":"./setting/fee-list.html", "date":"2025-08-10" },
+  { "cate":"m012", "dep01":"설정", "dep02":"약관", "url":"./setting/term-register.html", "date":"2025-08-10" },
+
+  { "cate":"m013", "dep01":"0925추가", "dep02":"청구완료", "url":"./0925/claim-completed.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"청구대기", "url":"./0925/claim-wait.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"배차취소(콜)", "url":"./0925/dispatch-cancel01.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"배차취소", "url":"./0925/dispatch-cancel02.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"배차중", "url":"./0925/dispatch-ing.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"배차대기(콜)", "url":"./0925/dispatch-wait01.html", "date":"2025-09-26" },
+  { "cate":"m013", "dep01":"0925추가", "dep02":"배차대기", "url":"./0925/dispatch-wait02.html", "date":"2025-09-26" },
+
+  { "cate":"m014", "dep01":"1017추가", "dep02":"배차중 상세", "url":"./1017/basech-detail.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]청구기준 설정", "url":"./1017/popup-billing-setting.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]차량교체", "url":"./1017/popup-car-change-form.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]차량반납", "url":"./1017/popup-car-return.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]차량선택", "url":"./1017/popup-car-select.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]청구서류 선택", "url":"./1017/popup-doc-select.html", "date":"2025-10-17" },
+  { "cate":"m014", "dep01":"1017추가", "dep02":"[팝업]업체요금표 기준가", "url":"./1017/popup-price-change.html", "date":"2025-10-17" },
+
+
+
+
+];
+
+ 
+$(function(){   
+	listTable(".siteNavi li", ".siteNavi li .num");
+}); 
+ 
+function listTable(cls, num){   
+	var tr;
+	for(i=0; i<work.length; i++){
+		tr += "<tr class="+work[i].cate+">";
+		tr += "<td>"+work[i].dep01+"</td>";
+		tr += "<td>"+work[i].dep02+"</td>";
+		tr += "<td><a href='./"+work[i].url+"' target='_blank'>"+work[i].url+"</a></td>";
+		tr += "<td class='ac'>"+work[i].date+"</td>";
+		tr += "</tr>"; 
+	}  
+	$("table tbody").append(tr);  
+	
+	$(num).each(function(z){
+		if(z===0){
+			$(num).eq(z).text("("+work.length+"p)");
+		}else{
+			$(num).eq(z).text("("+$('.m0'+z).length+"p)");
+		} 
+	}); 
+	$("body").on("click",cls, function(){
+		$(cls).removeClass("on"); 
+		$(this).addClass("on");
+		$("table tbody tr").hide();
+		if($(this).index() === 0){
+			$("table tbody tr").show();
+		}else{
+			$(".m0"+$(this).index()).show();
+		} 
+	});  
+}  
